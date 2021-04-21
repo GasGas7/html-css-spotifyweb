84ed14fca06a20f799345e890ff529f705a18faa
GasGas7 <diego.gastaldi.95@gmail.com>
2021-04-21 16:15:54 +0200

    Some changes with JavaScript

---
+++ assets/js/up.js
@@ -0,0 +1,41 @@
+
+/* Alert della pagina */
+
+alert("Questa riproduzione di Spotify è pazzesca!! Con JavaScript lo è ancora di più !! Click su'OK' per visualizzarla!! :D ");
+
+/* Popular artist getElementById */
+
+document.getElementById("pop_art").innerHTML="Artisti Popolari con JS";
+
+/* Ludovico Einaudi getElementById */
+
+document.getElementById("einaudi").innerHTML="Ludovico Einaudi con JS";
+
+/* Upgrade Button getElementById */
+
+document.getElementById("up_grd_btn").innerHTML="Effettua l'upgrade con JavaScript!!!!";
+
+
+/* ******Sidebar Menu changes****** */
+
+//Home
+
+document.getElementById("hometxt").innerHTML="Home with JS";
+
+//Search 
+
+document.getElementById("search").innerHTML="Search with JS";
+
+//Library
+
+document.getElementById("library").innerHTML="Library with JS";
+
+/* User changes */
+
+document.getElementById("john_doe").innerHTML="John Doe diventa Don Joe con JS";
+
+/* ******Sidebar Menu changes****** */
+
+/* A Cure for wellnes change  */
+
+document.getElementById("not_js").innerHTML="(Not made by Javascript ;'D....i feel ''valley of despair'' is so close)";
